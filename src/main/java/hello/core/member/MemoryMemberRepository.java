adb7001e59a0f766180fc684fda94b72745a849e
moon <cogus2010@naver.com>
2024-01-30 00:16:54 +0900

    2024-01-30 마무리!

--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -1,11 +1,13 @@
 package hello.core.member;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Component
+@Primary // 오류나서 잠깐 넣어둠 강의엔 X
 public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long, Member> store = new HashMap<>();
